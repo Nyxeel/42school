b27ee3b619e713d903a340e59ebf2c812e1fe1d7
Nyxeel <nyxeel@protonmail.com>
2025-04-24 08:49:43 +0200

    ft_memmove erstellt

---
+++ libft/ft_memmove.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char buffer[n];
+	unsigned char *ziel;
+	unsigned char *quelle;
+
+	i = 0;
+	ziel = (unsigned char *) dest;
+	quelle = (unsigned char *) src;
+
+	while (i < n) // Zwischenspeichern in buffer
+	{
+		buffer[i] = quelle[i];
+		i++;
+	}
+	printf("Buffer = %s\n", buffer);
+
+	i = 0;
+	while (i < n) // Überschreiben
+	{
+		ziel[i] = buffer[i];
+		i++;
+	}
+	return (dest);
+}
+
+int	main(void)
+{
+	char arr[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '\0'};
+	char array[] = "ABCDEFGH";
+
+	(char *) ft_memmove(&arr[4], &arr[0], 2);
+	printf("%s\n", arr);
+	memmove(&array[4], &array[0], 2);
+	printf("%s", &array[0]);
+}
