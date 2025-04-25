904b924f3a36259ec2188cac0cc2647ebe5f7568
Nyxeel <nyxeel@protonmail.com>
2025-04-25 21:06:49 +0200

    added ft_strnstr.c

--- Common Core/libft/ft_strnstr.c
+++ Common Core/libft/ft_strnstr.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
 
-int main() {
-    const char *text = "Hi schöne Welt";
+char *ft_strnstr(const char *haystack, const char *needle, size_t n)
+{
+    //unsigned char *haufen;
+    //unsigned char *nadel;
+    size_t i;
+    size_t j;
 
-    printf("Adresse von text     : %p\n", (void *)text);
-    printf("Adresse von text + 5 : %p\n", (void *)(text + 5));
-    printf("Inhalt ab text + 5   : %s\n", text + 5);
-    printf("text[5]              : %c\n", text[5]);
+    //haufen = (unsigned char) haystack;
+    //nadel = needle;
 
-    return 0;
+    i = 0;
+    j = 0;
+
+    if (needle[0] == NULL)
+        return ((char *)haystack);
+    while(i < n && haystack[i] != '\0')
+    {
+        printf("%zu\n", i);
+        while (haystack[i + j] == needle[j] && i + j < n)
+        {
+            printf("%zu\n", j);
+            if (needle[j + 1] == '\0')
+                return ((char *)haystack + i);
+            j++;
+        }
+        i++;
+    }
+    return (NULL);
+}
+
+int main(void) 
+{
+    
+    const char big[] = "Hi schöne Welt";
+    const char little[] = "Welt ";
+
+    printf("%s", ft_strnstr(big, little, 100));
 }
