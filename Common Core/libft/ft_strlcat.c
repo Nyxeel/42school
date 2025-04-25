73f46d5106f390033af6ee9ed017d586a1f9a889
Nyxeel <nyxeel@protonmail.com>
2025-04-25 13:15:59 +0200

    Added strlcat.c

--- Common Core/libft/ft_strlcat.c
+++ Common Core/libft/ft_strlcat.c
@@ -14,28 +14,25 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t i;
 	size_t j;
+	size_t buffer;
 	
-
+	if (ft_strlen(dst) >= size)
+		buffer = size + ft_strlen(src);
+	else
+		buffer = ft_strlen(dst) + ft_strlen(src);
 	i = 0;
 	while (dst[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	//printf("1: %zu\n", ft_strlen(dst));
 	while (src[j] != '\0' && i + j < size - 1)
 	{
 		dst[i + j] = src[j];
 		j++;
 	}
 	dst[i + j] = '\0';
-	i = 0;
-	while (src[j + i] != '\0')
-		i++;
-	//printf("2: %zu\n", ft_strlen(dst));
-	
-	return (ft_strlen(dst) + i + 1);
-
+	return (buffer);
 }
 
 int	main(void)
