8e3c1c3850750d7cd73dac751ba08283206cbf4b
Nyxeel <nyxeel@protonmail.com>
2025-04-28 12:44:23 +0200

    Started ft_split

---
+++ Common Core/libft/ft_split.c
@@ -0,0 +1,13 @@
+char **ft_split(char const *s, char c)
+{
+
+}
+
+int	main(void)
+{
+	char str[] = " Hallo, das ist ein Text zum Splitten, tschau!!!";
+	char sep = ' ';
+
+	char **split = ft_split(str, sep);
+
+}
