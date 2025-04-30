f247e102296480ca0e1363f9623e87240b7f5ee8
Nyxeel <nyxeel@protonmail.com>
2025-04-30 09:14:13 +0200

    uploaded unfinished ft_itoa.c

---
+++ Common Core/libft/ft_itoa.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static int ft_countdigit(int n)
+{
+	int digits;
+	long int num;
+
+	num = n;
+
+	digits = 0;
+	if (num == 0)
+		digits = 1;
+
+	if (num < 0)
+		num = -num;
+	while(num > 0)
+	{
+		num = num / 10;
+		digits++;
+	}
+	return (digits);
+}
+
+static char *ft_intochar(char *arr, long int num, int digits, int minus)
+{
+	int index;
+	
+	index = digits + minus - 1;
+	arr[digits] = '\0';
+	printf("minus = %d\n", minus); 
+	while (index >= 0)
+	{
+		arr[index] = (num % 10) + '0';
+		printf("intarr[%d]: %d\n", index, arr[index]);
+		num = num / 10;
+		index--;
+	}
+	if (minus == 1)
+			arr[0] = '-';
+	return (arr);
+}
+
+char *ft_itoa(int n)
+{
+	char *intarr;
+	int digits;
+	int minus;
+	long int num;
+
+	num = n;
+	minus = 0;
+	digits = ft_countdigit(n);
+	printf("digits: %d\n", digits);
+	if (num < 0)
+	{
+		num *= -1;
+		minus = 1;
+	}	
+	intarr = (char *)malloc((digits + minus + 1)* sizeof(char));
+	if (!intarr)
+		return (NULL);
+	intarr = ft_intochar(intarr, num, digits, minus);
+	return(intarr);
+}
+
+int	main(void)
+{
+	long int ascii = 2147483648;
+	char *converted;
+	
+	converted = ft_itoa(ascii);
+	printf("%s", converted);
+	free(converted);
+
+}
