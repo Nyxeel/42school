8c70232e8337bc2d08d836be53e2645ef09c92a8
Nyxeel <nyxeel@protonmail.com>
2025-05-09 13:42:52 +0200

    finished ft_memcpy.c

--- Common Core/libft/ft_memcpy.c
+++ Common Core/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 13:38:28 by pjelinek          #+#    #+#             */
-/*   Updated: 2025/05/09 13:41:18 by pjelinek         ###   ########.fr       */
+/*   Updated: 2025/05/09 13:42:35 by pjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return ((void *)dest);
 }
-
 
 /* int	main(void)
 {
