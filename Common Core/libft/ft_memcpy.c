c71ba0183772e33ed0f6d7cba228637bb30d64cc
Nyxeel <nyxeel@protonmail.com>
2025-05-09 13:40:43 +0200

    finished ft_memcpy.c

--- Common Core/libft/ft_memcpy.c
+++ Common Core/libft/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: netrunner <netrunner@student.42.fr>        +#+  +:+       +#+        */
+/*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 13:38:28 by pjelinek          #+#    #+#             */
-/*   Updated: 2025/05/09 13:30:14 by netrunner        ###   ########.fr       */
+/*   Updated: 2025/05/09 13:40:02 by pjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*ziel;
 	unsigned char		*quelle;
 
-	if (n == 0)
-		return (dest);
-	if (dest == NULL || src == NULL)
-		return (NULL);
 	ziel = (unsigned char *) dest;
 	quelle = (unsigned char *) src;
 	i = 0;
@@ -34,13 +30,13 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 }
 
 
-int	main(void)
+/* int	main(void)
 {
 	char *p;
 
 	p = ft_memcpy(NULL, NULL, 0);
 
 	printf("%s", p);
-/* 	char *c = memcpy("ERROR", "hi how are you", 5);
-	printf("%s", c); */
-}
+	/* char *c = memcpy(NULL, NULL, 2);
+	printf("%s", c);*/
+} */
