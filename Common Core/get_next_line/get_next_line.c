67263678bafcf59dd047b642585adb5d391700f0
Nyxeel <nyxeel@protonmail.com>
2025-05-14 23:47:04 +0200

    get next line update

--- Common Core/get_next_line/get_next_line.c
+++ Common Core/get_next_line/get_next_line.c
@@ -6,66 +6,199 @@
 /*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:43:08 by pjelinek          #+#    #+#             */
-/*   Updated: 2025/05/13 18:58:06 by pjelinek         ###   ########.fr       */
+/*   Updated: 2025/05/14 23:45:19 by pjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 1
 
-#include <fcntl.h>   // open
-#include <stdlib.h>  // free
-#include <stdio.h>   // printf
+# include <fcntl.h>   // open
+# include <stdlib.h>  // free
+# include <stdio.h>   // printf
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <string.h>
+# include <stdint.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+void	*ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*arr;
+
+	i = 0;
+	arr = (unsigned char *) s;
+	while (i < n)
+	{
+		arr[i] = 0;
+		i++;
+	}
+	return ((void *) s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*p;
+	size_t			total;
+
+	if (size == 0 || nmemb == 0)
+		return (malloc(0));
+	if (nmemb > (size_t)SIZE_MAX / size)
+		return (NULL);
+	total = nmemb * size;
+	p = malloc(total);
+	if (p == NULL)
+		return (NULL);
+	return (ft_bzero(p, total));
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+	size_t	str_len;
+
+	sub = NULL;
+	str_len = ft_strlen(s);
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= str_len || len == 0)
+		return (ft_calloc(1, sizeof(char)));
+	if (len > str_len - start)
+		len = str_len - start;
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	while (s[start + i] != '\0' && i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+/* char	*ft_trim(char *newline)
+{
 
 
-char *get_next_line(int fd)
+} */
+
+size_t	find_line(char const *brain, char const newline)
+{
+	size_t	i;
+
+	i = 0;
+	while (brain[i] != '\0')
+	{
+		if (brain[i] == newline)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	join = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!join)
+		return (NULL);
+	while (s1[i])
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		join[i + j] = s2[j];
+		j++;
+	}
+	join[i + j] = '\0';
+	return (join);
+}
+
+char	*ft_new(int fd)
 {
 	int			bytes;
 	char static	*brain;
-	int static	index;
+	char		*buffer;
+	int			index;
 
+	if (!brain)
+		brain = ft_calloc(0, 0);
 	bytes = 0;
-	while (brain[BUFFER_SIZE - 1] == '\n')
+	while (find_line(brain, '\n'))
 	{
-		brain[BUFFER_SIZE] = read(fd, BUFFER_SIZE);
-		index++;
+		buffer = (char *)malloc(BUFFER_SIZE + 1);
+		if (!buffer)
+			return (NULL);
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (!bytes)
+			return (free(buffer), ft_substr(brain, 0, index - 1));
+		buffer[bytes] = '\0';
+		brain = ft_strjoin(brain, buffer);
+		free(buffer);
 	}
-
-
-
-		return (strdup("Zeile5")) --> malloc - Rückgabe
+	index = find_line(brain, '\n');
+	char *line = ft_substr(brain, 0, index - 1);
+	char *rest = ft_substr(brain, index, ft_strlen(brain) - (index - 1));
+	free(brain);
+	brain = rest;
+	return (line);
 }
 
+char	*get_next_line(int fd)
+{
+	char		*newline;
 
-
-Read liest in Buffer bis \n
-static char *brain;
-speichert aktuelle line in static char *brain.
-
-substring von start bis inklusive \n extrahiert die
-aktuelle Zeiile und returned diese
-
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	newline = ft_new(fd);
+	if (!newline)
+		return (NULL);
+	//ft_trim(newline);
+	return (newline);
+}
 
 int	main(void)
 {
-	int fd = open("aot.txt", O_RDONLY);
-	if (fd == -1)
-		return (NULL);
+		int fd = open("aot.txt", O_RDONLY);
+		if (fd == -1)
+			return (0);
+		char *line;
 
-	int i = 1;
-	char *line;
+		int i = 0;
 
-	while ((line = get_next_line(fd)))
-	{
-		if (i == 5)
+
+		while ((i < 2))
 		{
-			printf("Zeile 5: %s", line);
+			line = get_next_line(fd);
+			if (!line)
+				return (0);
+			printf("%s", line);
 			free(line);
-			break;
+			i++;
 		}
-		free(line);
-		i++;
-	}
 	close(fd);
 	return (0);
 }
--- Common Core/get_next_line/get_next_line_utils.c
+++ Common Core/get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:38:47 by pjelinek          #+#    #+#             */
-/*   Updated: 2025/05/13 18:39:15 by pjelinek         ###   ########.fr       */
+/*   Updated: 2025/05/14 19:34:38 by pjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,69 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (p == NULL)
 		return (NULL);
 	return (ft_bzero(p, total));
+}
+
+
+
+int	ft_newline(char const *brain, char const newline)
+{
+	int	i;
+
+	i = 0;
+	while (brain[i] != '\0')
+	{
+		if (brain[i] == newline)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char	*ft_strjoin_free(char const *buffer, char const *brain)
+{
+	char	*join;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!buffer || !brain)
+		return (NULL);
+	join = (char *)ft_calloc(ft_strlen(buffer) + ft_strlen(brain) + 1);
+	if (!join)
+		return (NULL);
+	while (buffer[i])
+	{
+		join[i] = buffer[i];
+		i++;
+	}
+	j = 0;
+	while (brain[j])
+	{
+		join[i + j] = brain[j];
+		j++;
+	}
+	join[i + j] = '\0';
+	free(buffer);
+	return (join);
+}
+
+char	*ft_strdup_free(const char *buffer)
+{
+	int		i;
+	char	*brain;
+
+	if (src == NULL)
+		return (NULL);
+	brain = (char *)ft_calloc(sizeof(char) * (ft_strlen(buffer) + 1));
+	if (brain == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != 0)
+	{
+		brain[i] = src[i];
+		i++;
+	}
+	brain[i] = '\0';
+	free(buffer);
+	return (brain);
 }
