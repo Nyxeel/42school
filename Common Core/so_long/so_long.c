3b41ea4848061e5f7c2776ca0bd7bae3650ef37b
Nyxeel <nyxeel@protonmail.com>
2025-08-05 20:48:27 +0200

    input parsing done - floodfill still left

--- Common Core/so_long/so_long.c
+++ Common Core/so_long/so_long.c
@@ -3,29 +3,161 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: netrunner <netrunner@student.42.fr>        +#+  +:+       +#+        */
+/*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 11:15:57 by netrunner         #+#    #+#             */
-/*   Updated: 2025/08/05 11:55:21 by netrunner        ###   ########.fr       */
+/*   Updated: 2025/08/05 20:47:51 by pjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void  flood_fill(char **tab, t_point size, t_point begin)
-{
 
+int	grid_line_count(char **split)
+{
+	int	count;
+
+	count = 0;
+	if (split || *split)
+	{
+		while (split[count] != NULL)
+		count++;
+	}
+	return (count);
+}
+
+void	exit_call(char *message, char **split)
+{
+	write(2, "Error: ", 7);
+	write(2, message, ft_strlen(message));
+	if (split || *split)
+	ft_freeall(split, grid_line_count(split));
+	exit(1);
+}
+
+void	free_function(char **p)
+{
+	if (!*p)
+	return ;
+	free(*p);
+	*p = NULL;
+}
+
+char	**parse_input(int fd)
+{
+	int		i;
+	char	*line;
+	int		bytes;
+	char	**map;
+
+	line = (char *)ft_calloc(1, BUFFER_SIZE + 1);
+	if (!line)
+	return (0);
+	bytes = read(fd, line, BUFFER_SIZE);
+	if (bytes < 0)
+	return (free_function(&line), NULL);
+	if (bytes == 0)
+	return (free_function(&line), NULL);
+	line[bytes] = '\0';
+	map = ft_split(line, '\n');
+	if (!map)
+	return (free_function(&line), NULL);
+	free(line);
+	return (map);
+}
+
+void  flood_fill(char **map, t_coord length, t_coord begin)
+{
+	int	x;
+	int y;
+
+	if ()
+}
+
+int	check_walls(char **grid, t_data *game)
+{
+	int	i;
+
+	i = 0;
+	game->length.y = grid_line_count(grid);
+	while (grid[i])
+	{
+		if (i == 0 || i == game->length.y - 1)
+		{
+			if (ft_strcheck(grid[i++], '1'))
+			continue ;
+			else
+			return (0);
+		}
+		else
+		{
+			if (grid[i++][0] == '1' && grid[i][game->length.x - 1])
+				continue ;
+			else
+				return (0);
+		}
+	}
+	return (1);
+}
+
+int	check_rectangular(char **map, int line_length)
+{
+	int	i;
+
+	i = 1;
+	while (map[i])
+	{
+		if (ft_strlen(map[i++]) == line_length)
+			continue ;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+void	check_map(char *map_path, t_data *_game)
+{
+	int		fd;
+	int		line_length;
+
+	fd = open(map_path, O_RDONLY);
+	if (fd == EOF)
+		exit_call("Nothing found - Check path again", _game->map);
+	if (fd == 0)
+		exit_call("Map file empty", _game->map);
+	_game->map = parse_input(fd);
+	if (!_game->map)
+		exit_call("Map parsing failed", _game->map);
+	_game->length.x = ft_strlen(_game->map[0]);
+	if (!check_rectangular(_game->map, _game->length.x))
+		exit_call("Map is not rectangular", _game->map);
+	if (!check_walls(_game->map, _game))
+		exit_call("Map not proper walled", _game->map);
+	if (!flood_fill(_game->map, _game->length, _game->length))
+		exit_call("Floodfill failed", _game->map);
+
+	printf("OK");
 }
 
 
-int main(void)
+int main(int ac, char **av)
+{
+	t_data	*_game;
+	_game = ft_calloc(1, sizeof(t_data));
+	if (ac == 2)
+		check_map(av[1], _game);
+	else
+		write(2, "Error, no map found!", 21);
+}
+
+/* int main(void)
 {
 	char **area;
 	t_point grid;
 	t_point start_point;
 
 	void *mlx = mlx_init();
-	
+
 	void *win = mlx_new_window(mlx, width, height, "so_long");
 
 	int img_width, img_height;
@@ -33,8 +165,8 @@ int main(void)
 
 	mlx_put_image_to_window(mlx, win, img, x * TILE_SIZE, y * TILE_SIZE);
 
-	
-	
+
+
 
 	validate_map()
 	print_tab(area);
@@ -42,13 +174,41 @@ int main(void)
 	putc('\n');
 	print_tab(area);
 	return (0);
-}
+} */
 
 
-/* Prüfe, ob die Map rechteckig ist
-(jede Zeile gleich lang!)
 
-Prüfe, ob die Map komplett von '1' (Walls) umschlossen ist
+/*
+	ALLOWED FUNCTIONS:
 
-Prüfe, ob alle Ziele erreichbar sind
-(z.B. ob von 'P' aus zu allen 'C' und zum 'E' ein Pfad existiert) */
+	open, close, read, write,
+	malloc, free, perror,
+	strerror, exit
+
+	•All functions of the math
+	library (-lm compiler option,
+	man man 3 math)
+
+	•All functions of the MiniLibX
+
+
+
+
+
+CHECK LINE 1 and Last LINE are alle "1",
+Check the lines intbetween have at index 1 and last index "1"
+
+
+
+•You must verify if there is a valid path in the map.
+
+-FLOODFILL all "0" "C" "P" "C" ;
+
+•You must be able to parse any kind of map, as long as it respects the above rules.
+
+Open map and read it!
+	open();
+	read();
+
+
+ */
--- Common Core/so_long/so_long.h
+++ Common Core/so_long/so_long.h
@@ -1,5 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 19:16:59 by pjelinek          #+#    #+#             */
+/*   Updated: 2025/08/05 19:54:05 by pjelinek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100000
+# endif
+
 
 # include <limits.h>
 # include <stdbool.h>
@@ -9,12 +26,31 @@
 # include <stdlib.h>
 # include <stdint.h>
 # include <fcntl.h>
+# include <errno.h>
 
-
-typedef struct  s_point
+typedef struct s_coord
 {
-	int           x;
-	int           y;
-}               t_point;
+	int	x;
+	int	y;
+}	t_coord;
+
+typedef struct s_data
+{
+	size_t	movements;
+	void	*exit;
+	void	*coins;
+	char	**map;
+	t_coord	player;
+	t_coord	enemy;
+	t_coord	length;
+
+}	t_data;
+
+
+char	**ft_split(char const *s, char c);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_freeall(char **freearr, size_t index);
+size_t	ft_strlen(const char *str);
+int		ft_strcheck(const char *str, int c);
 
 #endif
