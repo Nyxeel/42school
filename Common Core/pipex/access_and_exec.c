600a6de796518bd0eae8fcf3499113bfda3634b3
Nyxeel <nyxeel@protonmail.com>
2025-09-08 14:57:44 +0200

    pipex -exit codes handled

--- Common Core/pipex/access_and_exec.c
+++ Common Core/pipex/access_and_exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   access_and_exec.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pjelinek <pjelinek@student.42.fr>          +#+  +:+       +#+        */
+/*   By: netrunner <netrunner@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 15:12:55 by pjelinek          #+#    #+#             */
-/*   Updated: 2025/09/05 18:47:10 by pjelinek         ###   ########.fr       */
+/*   Updated: 2025/09/08 14:13:23 by netrunner        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,13 +73,29 @@ static void	direct_access(t_data *pipex, char *command)
 	if (access(command, X_OK) == -1)
 	{
 		if (errno == EACCES)
-			free_split_exit(126, "1 denied", pipex);
-		free_split_exit(127, " : no command found\n", pipex);
+			free_split_exit(126, " : permission denied\n", pipex);
+		if (errno == ENOENT)
+		{
+			write(2, pipex->cmd_split[0], ft_strlen(pipex->cmd_split[0]));
+			perror(" ");
+			free_split_exit(127, NULL, pipex);
+		}
+		if (errno == ENOTDIR)
+		{	write(2, pipex->cmd_split[0], ft_strlen(pipex->cmd_split[0]));
+			perror(" ");
+			free_split_exit(126, NULL, pipex);
+		}
 	}
 	execve(command, pipex->cmd_split, pipex->path);
 	if (errno == ENOENT || errno == ENOTDIR)
 		free_split_exit(127, "WRONG", pipex);
-	free_split_exit(1, "HELLO", pipex);
+	if (errno == ENOEXEC)
+	{
+		write(2, pipex->cmd_split[0], ft_strlen(pipex->cmd_split[0]));
+		perror(" ");
+		free_split_exit(126, NULL, pipex);
+	}
+	free_split_exit(126, " : is a directory\n", pipex);
 }
 
 static void	search_path_access(t_data *pipex, char *full_path)
@@ -125,7 +141,7 @@ void	find_access(t_data *pipex, char *command)
 		direct_access(pipex, pipex->cmd_split[0]);
 	pipex->access_path = find_path(pipex->path);
 	if (!pipex->access_path)
-		free_split_exit(126, "XXXXXXXXXX", pipex);
+		free_split_exit(127, " : command not found\n", pipex);
 	i = 0;
 	while (pipex->access_path[i])
 	{
@@ -136,9 +152,7 @@ void	find_access(t_data *pipex, char *command)
 		free(path);
 		i++;
 	}
-	write(2, pipex->cmd_split[0], ft_strlen(pipex->cmd_split[0]));
-	free_split_exit(126, "NNOT NOT NOT NOT", pipex);
-
+	free_split_exit(127, " : command not found\n", pipex);
 }
 
 
